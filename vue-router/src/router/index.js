276742bf882dc21c69f738a7ea10eed96506dbd6
ayapes <web@ymok.me>
2023-01-17 15:41:35 +0900

    2023-01-17-02

--- vue-router/src/router/index.js
+++ vue-router/src/router/index.js
@@ -11,12 +11,13 @@ const routes = [
         path:'/about',
         name:'About',
         component:()=>import('../views/About.vue')
-    }
+    },
+    // 以下リンク増えるたびに書き加える
 ]
 
 const router = createRouter({
     history:createWebHistory(process.env.BASE_URL),
     routes
 })
-
+// main.jsでimportします
 export default router
