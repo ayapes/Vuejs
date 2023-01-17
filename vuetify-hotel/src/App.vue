ca631aaac91e3f921d147f347190da0d9c43179a
ayapes <web@ymok.me>
2023-01-17 14:29:32 +0900

    2023-01-17

---
+++ vuetify-hotel/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+
+  <v-app id="inspire">
+    <v-spacer></v-spacer>
+    <v-img src="@/assets/logo.png" />
+    <v-app-bar class="px-3" color="white" flat density="compact" extension-height="48">
+
+
+      <v-spacer></v-spacer>
+
+      <v-tabs centered color="grey-darken-2">
+        <v-tab v-for="link in links" :key="link">
+          <img :src="link.src">
+        </v-tab>
+      </v-tabs>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-main class="bg-grey-lighten-3">
+      <v-container>
+        <v-row>
+          <v-col cols="12" sm="2">
+            <v-sheet rounded="lg" min-height="268">
+              <!--  -->
+            </v-sheet>
+          </v-col>
+
+          <v-col cols="12" sm="8">
+            <v-sheet min-height="70vh" rounded="lg">
+              <!--  -->
+            </v-sheet>
+          </v-col>
+
+          <v-col cols="12" sm="2">
+            <v-sheet rounded="lg" min-height="268">
+              <!--  -->
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    links: [
+      { src: require("@/assets/logo.png") },
+    ],
+  }),
+}
+</script>
--- vuetify-project/src/App.vue
+++ vuetify-project/src/App.vue
@@ -1,7 +1,70 @@
 <template>
-  <router-view />
-</template>
+  <v-app>
+    <v-navigation-drawer color="primary">
+      <div class="pa-5">
+        <h2>ナビゲーションバー</h2>
+        <div class="pt-2">
+          <p>
+            この部分は画面サイズが1280pxより大きい時のみ表示される設定です。
+          </p>
+        </div>
+      </div>
+    </v-navigation-drawer>
 
-<script setup>
-  //
+    <v-app-bar color="accent" absolute="true">
+      <div class="pa-5">
+        <h2>ヘッダー</h2>
+        <p>この部分は「headerタグ」に変換されます。</p>
+      </div>
+    </v-app-bar>
+
+    <!-- style属性で通常のcssも利用できる -->
+    <v-main style="height: 200vh">
+      <v-container fluid>
+        <v-card class="mx-auto my-10">
+          <p>
+            「v-app-bar」と「v-footer」はデフォルトでは画面上下に固定されます(CSSのpositon="fixed"の状態)。
+          </p>
+          <p>「absolute="true"」属性を付けると、スクロールに従い移動します。</p>
+        </v-card>
+
+        <v-card class="mx-auto my-10">
+          <p>
+            「v-app-bar」と「v-footer」はデフォルトでは画面上下に固定されます(CSSのpositon="fixed"の状態)。
+          </p>
+          <p>「absolute="true"」属性を付けると、スクロールに従い移動します。</p>
+        </v-card>
+
+        <h3>公式から完コピのカルーセルパネルの例</h3>
+        <v-carousel>
+          <v-carousel-item
+            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+            cover
+          ></v-carousel-item>
+
+          <v-carousel-item
+            src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg"
+            cover
+          ></v-carousel-item>
+
+          <v-carousel-item
+            src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+            cover
+          ></v-carousel-item>
+        </v-carousel>
+      </v-container>
+    </v-main>
+
+    <v-footer app absolute="true" class="bg-teal">
+      <div class="pa-5">
+        <h2>フッター</h2>
+        <p>この部分は「footerタグ」に変換されます。</p>
+      </div>
+    </v-footer>
+  </v-app>
+</template>
+<script>
+export default {
+  name: "App",
+};
 </script>
